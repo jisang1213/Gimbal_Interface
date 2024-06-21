2e15f30bbe9d3acb5c77efec26cd02fca304ec68
Jisang <jisangyou1@gmail.com>
2024-06-21 18:49:06 +0900

    modified commander

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 ![](images/gimbal_raisim.png)
-## Usage
+## Overview
+
+This is a standalone ros2 node that is designed to run independently from raisin.
+It receives the robot state through the custom "robotstate" message and it sends the H matrix back to raisin through the "DoubleArray12" message.
 
 # commander node
-
 The commander node receives commands from raisin and publishes them to the "/gimbal_command" topic. There are two ways it generates commands.
-The first way is using a zero target (passive). The second way is by using the robot state (active).
 
 # gimbal_com node
 The gimbal_com node is responsible for the actual serial communication.
-It is subscribed to the "/gimbal_command" topic from which it receives roll/pitch/yaw commands (in the base frame) and publishes joint states to the "/gimbal_state" topic.
-The port name (std::string) should be passed to its constructor.
+It is subscribed to the "/gimbal_command" topic from which it receives roll/pitch/yaw commands (in the base frame) and publishes the information necessary for coordinate transformation to the "/gimbal_state" topic.
+The port name should be passed to its constructor.
 
 ```cpp
+std::string portname = "/dev/ttyACM0";
 auto gimbal_com = std::make_shared<gimbal_com>(portname);
 ```
 
@@ -26,8 +28,24 @@ gimbal.getHmatrix_L(); //for left camera
 gimbal.getHmatrix_R(); //for right camera
 ```
 
-The transformation matrix is published to the "railab_raibo/gimbal_state" topic as an array of 12 doubles.
-The first 9 elements are the rows of the rotation matrix. The remaining three elements represent the position vector of the cameras with respect to the origin of the base.
+The H matrix is published to the "railab_raibo/gimbal_state" topic as an array of 12 doubles.
+The first 9 elements are the rows of the rotation matrix [R1 R2 R3]. The remaining three elements represent the position vector of the cameras with respect to the origin of the base [x y z].
 
+The 4x4 H matrix is converted into an array as follows:
+```cpp
+//rotation
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        output_msg->transform_L[3*i + j] = H_matrix_left_cam(i,j);
+        output_msg->transform_R[3*i + j] = H_matrix_right_cam(i,j);
+    }
+}
+
+//position
+for(int i=0; i<3; i++){
+    output_msg->transform_L[9+i] = H_matrix_left_cam(i,3);
+    output_msg->transform_R[9+i] = H_matrix_right_cam(i,3);
+}
+```
 The origin of the base is the midpoint of the line connecting the two mounting holes.
 ![Base Origin](images/base_origin.png)
--- include/gimbal_interface/commander.hpp
+++ include/gimbal_interface/commander.hpp
@@ -13,30 +13,39 @@
 class gimbal_commander : public rclcpp::Node {
 public:
     gimbal_commander() : Node("gimbal_commander_node") {
-        publisher_ = this->create_publisher<geometry_msgs::msg::Vector3>("/gimbal_command", 10);
-        timer_ = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&gimbal_commander::publishMessage, this));
+
+        subscriber_ = create_subscription<gimbal_interface::msg::robotstate>(
+                "railab_raibo/gimbal_command", 10, std::bind(&gimbal_com::sendCommand, this, std::placeholders::_1));
+
+        publisher_ = create_publisher<geometry_msgs::msg::Vector3>("/gimbal_command", 10);
+        //timer_ = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&gimbal_commander::publishMessage, this));
     }
 
 private:
-    void publishMessage() {
-        
-        auto message = geometry_msgs::msg::Vector3();
+    void sendCommand(const gimbal_interface::msg::robotstate state) {
 
         // compute robot angular velocity in body frame
-        raibot->getState(genCo_, genVel_);
-        quat = genCo_.segment<4>(3);
-        raisin::robot::sensor::quatToRotMat(quat, rot);
-        angVelW = genVel_.segment<3>(3);    //world frame
+        quat[0] = state.quaternion.w;
+        quat[1] = state.quaternion.x;
+        quat[2] = state.quaternion.y;
+        quat[3] = state.quaternion.z;
+
+        angVelW[0] = state.angVelW.x;
+        angVelW[1] = state.angVelW.y;
+        angVelW[2] = state.angVelW.z;
+
+        rot = quat.toRotationMatrix();
+
         angVelB = rot.transpose() * angVelW; //body frame
 
-        raisin::robot::sensor::rotMatToZYXEuler(rot, eulerAng);
+        eulerAng = rot.eulerAngles(0, 1, 2);
         roll = eulerAng[0];
         pitch = eulerAng[1];
 
+        auto message = geometry_msgs::msg::Vector3();
         message.x = -roll;
         message.y = -pitch-0.2;
         message.z = -0.2*angVelB[2];//0.9*sin(count/300.0);
-        count++;
 
         // for testing
         // message.x = 0.0;
@@ -47,16 +56,14 @@ private:
         // Publish the message
         publisher_->publish(message);
     }
-    
+
+    rclcpp::Subscription<gimbal_interface::msg::robotstate>::SharedPtr subscriber_;
     rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
-    Eigen::VectorXd genCo_, genVel_;
-    Eigen::Vector4d quat;  // W --> B
-    Eigen::Vector3d pos;   // W --> B
-    Eigen::Vector3d linVelW, angVelW, angVelB, linAccB;
+    Eigen::Quaterniond quat;  // W --> B
+    Eigen::Vector3d angVelW, angVelB;
     Eigen::Matrix3d rot;  // W --> B
     Eigen::Vector3d eulerAng;
     double roll, pitch;
     int count = 0;
-    bool active_control = false;
 };
